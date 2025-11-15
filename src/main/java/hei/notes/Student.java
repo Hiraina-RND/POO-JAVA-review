95d0217b1837fbdc59b0bf3027669b96925925bc
Hiraina <hei.hiraina@gmail.com>
2025-11-15 20:05:35 +0300

    Add Student class with all required attributes

---
+++ src/main/java/hei/notes/Student.java
@@ -0,0 +1,20 @@
+package hei.notes;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+@AllArgsConstructor
+
+public class Student {
+    final private int id;
+    final private String firstName;
+    final private String lastName;
+    final private LocalDate birthDate;
+    final private String email;
+    final private String phoneNumber;
+    final private String group;
+    final private Tutor tutor;
+}
