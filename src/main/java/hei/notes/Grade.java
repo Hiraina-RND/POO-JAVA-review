ebd6e0f833c84049240a61b046d1bc5d464441ac
Hiraina <hei.hiraina@gmail.com>
2025-11-16 20:47:19 +0300

    add getExamGrade(Instant) method to retrieve grade at a specific time

--- src/main/java/hei/notes/Grade.java
+++ src/main/java/hei/notes/Grade.java
@@ -2,7 +2,9 @@ package hei.notes;
 
 import lombok.Getter;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,5 +27,22 @@ public class Grade {
     public void updateGrade(double newValue, String reason) {
         this.currentValue = newValue;
         this.histories.add(new NoteHistory(newValue, LocalDateTime.now(), reason));
+    }
+
+    public double getExamGrade(Instant t) {
+        for (NoteHistory noteHistory : histories) {
+
+            Instant dateAsInstant = noteHistory
+                    .getDate()
+                    .atZone(ZoneId.systemDefault())
+                    .toInstant();
+
+            if (dateAsInstant.equals(t)) {
+                return  noteHistory.getValue();
+            }
+        }
+        throw new IllegalArgumentException(
+                "No grade found for the specified instant."
+        );
     }
 }
